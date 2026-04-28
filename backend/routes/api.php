27e2a7b36f8ce5ae6cdbeeb5b861d83e60d08610
RanaHany8 <116173222+RanaHany8@users.noreply.github.com>
2026-04-28 19:00:21 +0300

    Update api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Validation\Rules;
-// ضيفي الـ Controllers بتوعك هنا فوق
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\JobController;
 

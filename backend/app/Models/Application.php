e4cc8a92b97afded732b221fe5b11dece1287c71
nancyjosf <your_email@example.com>
2026-05-04 18:06:53 +0300

    Create Application model

---
+++ backend/app/Models/Application.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Application extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['job_id', 'user_id', 'resume', 'cover_letter', 'status'];
+
+    public function job(): BelongsTo
+    {
+        return $this->belongsTo(JobListing::class, 'job_id');
+    }
+
+    public function candidate(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
